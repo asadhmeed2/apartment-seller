42a8ddcc8bb4ab7b9af3c0a4178fa47f764e849d
newasad <asadhmeed2@gmail.com>
2023-12-03 20:52:48 +0200

    ..

--- apt-filter.js
+++ apt-filter.js
@@ -2,7 +2,7 @@
 
 const must = (radio) => radio.filter(rb => rb.checked).length
 
-const findRelevantApts = function (address, minPrice, maxPrice, minRooms, maxRooms, immediate) {
+const findRelevantApts = function (address, minPrice, maxPrice, minRooms, maxRooms, immediate,parking) {
     let relevantApts = apartments.filter(a =>
         a.address.toLowerCase().includes(address.toLowerCase() || "") &&
         a.price >= (minPrice || 0) &&
@@ -11,6 +11,11 @@ const findRelevantApts = function (address, minPrice, maxPrice, minRooms, maxRoo
         a.rooms <= (maxRooms || a.rooms)
     )
 
-    immediate = !immediate[0].checked
-    return immediate ? relevantApts : relevantApts.filter(a => (a.immediate)) 
+    immediate = immediate[0].checked
+    parking = parking[0].checked
+
+    let results = !(immediate) ? relevantApts : relevantApts.filter(a => (a.immediate))
+    results = !parking ? results : results.filter(a => (a.parking))
+
+    return results;
 }
--- main.js
+++ main.js
@@ -1,3 +1,16 @@
+const apartmentsSource = $("#apartment-template").html();
+
+const apartmentTemplate = Handlebars.compile(apartmentsSource);
+
+const results = $("#results") 
+
+const renderApts = function (apartments) {
+    results.empty()
+    console.log(apartments) //array of apartments to render
+    
+    const newHtml = apartmentTemplate({apartments,thereIsResults:!apartments.length})
+    results.append(newHtml)
+}
 
 $("button").on("click", function () {
     let address = $("#addr-input").val()
@@ -6,15 +19,10 @@ $("button").on("click", function () {
     let minRooms = $("#min-r-input").val()
     let maxRooms = $("#max-r-input").val()
     let immediate = $("#immed-y")
+    let parking = $("#park-y")
 
-    let relevantApts = findRelevantApts(address, minPrice, maxPrice, minRooms, maxRooms, immediate)
+    let relevantApts = findRelevantApts(address, minPrice, maxPrice, minRooms, maxRooms, immediate, parking)
     renderApts(relevantApts)
 })
-
-const renderApts = function (apartments) {
-    $("#results").empty()
-    console.log(apartments) //array of apartments to render
-    //Your code goes here.
-}
 
 renderApts(apartments) //renders apartments when page loads
